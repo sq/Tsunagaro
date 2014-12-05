112c37f2a4b7953d68be9beeb6eea959736a1c7b
Katelyn Gadd <kg@luminance.org>
2014-12-05 04:50:55 -0800

    Scaffolding

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using Squared.Task;
+
+namespace Tsunagaro {
+    static class Program {
+        public static readonly TaskScheduler Scheduler = new TaskScheduler(JobQueue.WindowsMessageBased);
+
+        [MTAThread]
+        static void Main () {
+            UIMain();
+        }
+
+        private static void UIMain () {
+            using (var trayContextMenu = new ContextMenuStrip {
+                Items = {
+                    {"E&xit", null, (s, e) => Application.Exit()}
+                }
+            }) 
+            using (var trayIcon = new NotifyIcon {
+                ContextMenuStrip = trayContextMenu,
+                Text = "Tsunagaro",
+                Icon = Tsunagaro.Properties.Resources.TrayIcon
+            }) {
+                Scheduler.Start(
+                    StartupTask(
+                        () => trayIcon.Visible = true
+                    ), 
+                    TaskExecutionPolicy.RunAsBackgroundTask
+                );
+
+                Application.Run();
+            }
+        }
+
+        public static IEnumerator<object> StartupTask (Action ready) {
+            ready();
+
+            yield break;
+        }
+    }
+}
